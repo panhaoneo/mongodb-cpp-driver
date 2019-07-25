56127d2dd12566e142311190e82bc2bdfd431675
LeoFlower <1015081605@qq.com>
2019-07-25 16:51:05 +0800

    Update Mongo.h
    上传编译通过的demo

--- Mongo.h
+++ Mongo.h
@@ -6,8 +6,10 @@
 #include "util/tc_thread_queue.h"
 #include "util/tc_config.h"
 #include "Common.h"
+#include "boost/variant.hpp"
 
-#include <string>
+
+#include <iostream>
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/builder/basic/kvp.hpp>
 #include <bsoncxx/json.hpp>
@@ -15,19 +17,20 @@
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <mongocxx/pool.hpp>
-#include <mongocxx/uri.hpp>
+#include <mongocxx/exception/exception.hpp>
+
+
 
 using namespace::std;
+using std::string;
 using bsoncxx::builder::basic::kvp;
 
 struct NF_MongoDBConf
 {
-  
     string _host;
     string _user;
     string _password;
     string _port;
-    string _database;
 
     /**
     * @brief 构造函数
@@ -36,13 +39,12 @@ struct NF_MongoDBConf
     {
     }
 
-    void init(const string &hostip, const string &port, const string &username, const string& password, const string &dbname)
+    void init(const string &hostip, const string &port, const string &username, const string& password)
     {
         _host = hostip;
         _user = username;
         _password = password;
         _port = port;
-        _database = dbname;
     }
 
     /**
@@ -53,7 +55,6 @@ struct NF_MongoDBConf
     *        dbuser:用户名
     *        dbpass:密码
     *        dbport:端口
-    *        dbname:数据库名称
     */
     void loadFromMap(const map<string, string> &mpParam)
     {
@@ -63,7 +64,6 @@ struct NF_MongoDBConf
         _user        = mpTmp["dbuser"];
         _password    = mpTmp["dbpass"];
         _port        = mpTmp["dbport"];
-        _database    = mpTmp["dbname"];
 
         if(mpTmp["dbport"] == "")
         {
@@ -74,29 +74,40 @@ struct NF_MongoDBConf
 };
 
 
-class CNFMongo
+enum ValueType
+{
+	MONGODB_INT32,
+	MONGODB_INT64,
+	MONGODB_DOUBLE,
+	MONGODB_STRING,
+};
+
+typedef  boost::variant<int32_t,int64_t,double,std::string> ValueVariant;
+typedef map<string, pair<ValueType, ValueVariant> > MONGODB_RECORD_DATA;
+
+class CNFMongo 
 {
 public:
 	CNFMongo();
-	CNFMongo(const string &hostip, const string &port, const string &username, const string& password, const string &dbname);
+	CNFMongo(const string &hostip, const string &port, const string &username, const string& password);
     CNFMongo(const NF_MongoDBConf& nfDbConf);
 	~CNFMongo();
-    void init(const string &hostip, const string &port, const string &username, const string& password, const string &dbname);
-    void init(const NF_MongoDBConf& nfDbConf);
+    void Init(const string &hostip, const string &port, const string &username, const string& password);
+    void Init(const NF_MongoDBConf& nfDbConf);
 
-    void Connect();
-
-    enum ValueType
-    {
-        MONGODB_INT64,
-        MONGODB_DOUBLE,
-        MONGODB_STRING,
-    };
-    typedef map<string, pair<ValueType, string> > MONGODB_RECORD_DATA;
+    
 
     int UpdateRecord(const string &strAreaDb, const string &collection, const MONGODB_RECORD_DATA &mapData, const MONGODB_RECORD_DATA &mapFilter);
 
     int InsertRecord(const string &strAreaDb, const string &collection, const MONGODB_RECORD_DATA &mapData);
+
+private:
+	void connect();
+	bool get_client(mongocxx::pool::entry& dbClient);
+	bool try_get_client(mongocxx::stdx::optional<mongocxx::pool::entry>& dbClient);
+
+	void buildDoc(MONGODB_RECORD_DATA mapData,  bsoncxx::builder::basic::document &doc);
+	void buildIndexDoc(MONGODB_RECORD_DATA mapData,  bsoncxx::builder::basic::document &doc);
 
 private:
     std::unique_ptr<mongocxx::instance> _instance;
@@ -104,6 +115,7 @@ private:
     mongocxx::pool *_pMongoPoolptr;
     NF_MongoDBConf _dbConf;
 };
+
 
 #endif
 
