76a01b22b978279c8c6a149c9541ac29a74c3182
LeoFlower <1015081605@qq.com>
2019-07-31 21:30:26 +0800

    Update mongo.h

--- mongo.h
+++ mongo.h
@@ -8,7 +8,6 @@
 #include "Common.h"
 #include "boost/variant.hpp"
 
-
 #include <iostream>
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/builder/basic/kvp.hpp>
@@ -18,11 +17,10 @@
 #include <mongocxx/instance.hpp>
 #include <mongocxx/pool.hpp>
 #include <mongocxx/exception/exception.hpp>
-
-
+#include <mongocxx/options/update.hpp>
+#include <mongocxx/options/insert.hpp>
 
 using namespace::std;
-using std::string;
 using bsoncxx::builder::basic::kvp;
 
 struct NF_MongoDBConf
@@ -83,9 +81,11 @@ enum ValueType
 };
 
 typedef  boost::variant<int32_t,int64_t,double,std::string> ValueVariant;
+
+//mongo操作的数据  嵌套map key：存入mongodb的键  value：pair<ValueType, ValueVariant> 值的类型和数值
 typedef map<string, pair<ValueType, ValueVariant> > MONGODB_RECORD_DATA;
 
-class CNFMongo 
+class CNFMongo : public TC_Singleton<CNFMongo>
 {
 public:
 	CNFMongo();
@@ -95,20 +95,57 @@ public:
     void Init(const string &hostip, const string &port, const string &username, const string& password);
     void Init(const NF_MongoDBConf& nfDbConf);
 
-    
+    /**
+    * @brief 更新mongo单条数据记录 
+    * 
+    * @param strAreaDb  所属db
+    *        collection 所属collection
+    *        mapData    map<string, pair<ValueType, ValueVariant> >  存储的数据
+    *        mapFilter  map<string, pair<ValueType, ValueVariant> >  需要更新的项
+    * 
+    * @return 0-正常  -1-异常
+    */
+    int UpdateRecord(const string &strAreaDb, const string &collection, MONGODB_RECORD_DATA &mapData, MONGODB_RECORD_DATA &mapFilter);
 
-    int UpdateRecord(const string &strAreaDb, const string &collection, const MONGODB_RECORD_DATA &mapData, const MONGODB_RECORD_DATA &mapFilter);
+    /**
+    * @brief 插入mongo单条数据记录 
+    * 
+    * @param strAreaDb  所属db
+    *        collection 所属collection
+    *        mapData    map<string, pair<ValueType, ValueVariant> >  存储的数据
+    * 
+    * @return 0-正常  -1-异常
+    */
+    int InsertRecord(const string &strAreaDb, const string &collection, MONGODB_RECORD_DATA &mapData);
 
-    int InsertRecord(const string &strAreaDb, const string &collection, const MONGODB_RECORD_DATA &mapData);
+    /**
+    * @brief 创建单字段索引 
+    * 
+    * @param strAreaDb  所属db
+    *        collection 所属collection
+    *        mapData    map<string, pair<ValueType, ValueVariant> >  需要创建索引的列表
+    * 
+    * @return 0-正常  -1-异常
+    */
+    int BuildSingleIndex(const string &strAreaDb, const string &collection, MONGODB_RECORD_DATA &mapData);
+
+      /**
+    * @brief 简单doc查找
+    * 
+    * @param strAreaDb  所属db
+    *        collection 所属collection
+    *        mapData    map<string, pair<ValueType, ValueVariant> >  需要查找的条件
+    *        cursor     查找到的迭代器集合
+    * @return 0-正常  -1-异常
+    */
+    int FindRecord(const string &strAreaDb, const string &collection, MONGODB_RECORD_DATA &mapData ,mongocxx::cursor &cursor);
 
 private:
 	void connect();
 	bool get_client(mongocxx::pool::entry& dbClient);
 	bool try_get_client(mongocxx::stdx::optional<mongocxx::pool::entry>& dbClient);
 
-	void buildDoc(MONGODB_RECORD_DATA mapData,  bsoncxx::builder::basic::document &doc);
-	void buildIndexDoc(MONGODB_RECORD_DATA mapData,  bsoncxx::builder::basic::document &doc);
-
+	void buildDoc(MONGODB_RECORD_DATA& mapData,  bsoncxx::builder::basic::document &doc);
 private:
     std::unique_ptr<mongocxx::instance> _instance;
     bool _bConnect;
@@ -116,6 +153,7 @@ private:
     NF_MongoDBConf _dbConf;
 };
 
+#define MongoHandle CNFMongo::getInstance()
 
 #endif
 
